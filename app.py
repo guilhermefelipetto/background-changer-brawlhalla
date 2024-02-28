091b757c87824774a51e0338431546bb83278b76
guilhermefelipetto <highvoltagexbox@gmail.com>
2024-02-28 16:43:40 -0300

    Adicionando features de redimensionamento de imagem opcional

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 import tkinter as tk
 import webbrowser
-from tkinter import filedialog, messagebox, scrolledtext
-from tkinter.font import Font
-from image_utils import verificar_tamanho, substituir_imagens
+from tkinter import filedialog, messagebox
+from image_utils import verificar_tamanho, substituir_imagens, redimensionar_imagem
 
 class App:
     def __init__(self):
@@ -33,9 +32,18 @@ class App:
         self.btn_tutorial = tk.Button(self.root, text="Ver Tutorial", command=self.tutorial, width=25, height=1)
         self.btn_tutorial.pack(pady=10)
 
-        # Label para exibir o texto 'teste'
+        # Botao para redimensionar a imagem selecionada
+        self.opcao_redimensionar = tk.BooleanVar()
+        self.checkbox_redimensionar = tk.Checkbutton(self.root, text='Redimensionar antes de substituir', variable=self.opcao_redimensionar, onvalue=True, offvalue=False)
+        self.checkbox_redimensionar.pack()
+
+        # Botao para substituir as imagens
+        self.btn_substituir = tk.Button(self.root, text="Substituir Imagens", command=self.comando_substituir_imagens_com_opcao, width=25, height=1)
+        self.btn_substituir.pack(pady=10)
+
+        # Label de texto
         self.label_teste = tk.Label(self.root, text="Diretorio do jogo:")
-        self.label_teste.pack(pady=10)
+        self.label_teste.pack()
 
         # Text widget para exibir o caminho do diretório selecionado
         self.text_caminho = tk.Text(self.root, height=1, wrap='none')  # wrap='none' para desabilitar quebra de linha automática
@@ -44,7 +52,6 @@ class App:
         self.text_caminho.pack(fill='x')
         self.scrollbar_horizontal.pack(fill='x')
         
-    
     def tutorial(self):
         url_video = 'https://youtu.be/hdXtK3JhFes'
         webbrowser.open(url_video)
@@ -73,18 +80,38 @@ class App:
                 break  # Sai do loop, encerrando a função sem fazer mais nada
 
             if verificar_tamanho(imagem_substituta):
-                substituir_imagens(imagem_substituta, self.diretorio_brawlhalla)
-                messagebox.showinfo("Concluído", "As imagens foram substituídas com sucesso.")
+                self.imagem_selecionada = imagem_substituta
+                self.btn_substituir['state'] = 'normal'
                 break  # Sai do loop após a substituicao bem-sucedida
             else:
                 resposta = messagebox.askyesno("Tamanho da imagem incorreto",
                                             "A imagem selecionada nao tem o tamanho preferido de 2048x1151. "
                                             "Deseja continuar mesmo assim?")
                 if resposta:
-                    substituir_imagens(imagem_substituta, self.diretorio_brawlhalla)
-                    messagebox.showinfo("Concluído", "As imagens foram substituídas com sucesso.")
+                    self.imagem_selecionada = imagem_substituta
+                    self.btn_redimensionar['state'] = 'normal'
+                    self.btn_substituir['state'] = 'normal'
                     break  # Sai do loop apos a substituição bem-sucedida
                 # Se o usuario clicar em "Nao", o loop continua, pedindo uma nova seleção de imagem
+        
+    def comando_substituir_imagens_com_opcao(self):
+        if self.opcao_redimensionar.get():
+            if hasattr(self, 'imagem_selecionada') and self.imagem_selecionada:
+                try:
+                    redimensionar_imagem(self.imagem_selecionada)
+                    messagebox.showinfo('Sucesso', 'Imagens substituida com sucesso.')
+                except Exception as e:
+                    messagebox.showerror('Erro', f'Falha ao redimensionar imagem: {e}')
+                    return
+        
+        if hasattr(self, 'imagem_selecionada') and self.imagem_selecionada and self.diretorio_brawlhalla:
+            try:
+                substituir_imagens(self.imagem_selecionada, self.diretorio_brawlhalla)
+                messagebox.showinfo('Concluído', 'As imagens foram substituídas com sucesso.')
+            except Exception as e:
+                messagebox.showerror('Erro', f'Falha ao substituir imagens: {e}')
+        else:
+            messagebox.showerror('Erro', 'Operacao Inválida. Verifique se a imagem foi selecionada e o diretório do jogo definido.')
 
 
     def run(self):
--- image_utils.py
+++ image_utils.py
@@ -6,6 +6,7 @@ def verificar_tamanho(imagem_path):
     with Image.open(imagem_path) as img:
         return img.size == (2048, 1151)
 
+
 def substituir_imagens(imagem_substituta, diretorio):
     if os.path.exists(diretorio):
         arquivos = os.listdir(diretorio)
@@ -15,3 +16,9 @@ def substituir_imagens(imagem_substituta, diretorio):
                 shutil.copy(imagem_substituta, caminho_completo)
     else:
         raise FileNotFoundError("Diretório não encontrado.")
+
+
+def redimensionar_imagem(imagem_path, largura=2048, altura=1151):
+    with Image.open(imagem_path) as img:
+        img_redimensionada = img.resize((largura, altura), Image.Resampling.LANCZOS)
+        img_redimensionada.save(imagem_path)
